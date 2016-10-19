caf88c2cbf2063f4898bbc8bf0c50e82f2f42150
Artur Rosa <rossinek@gmail.com>
2016-10-19 13:45:04 +0200

    edge based demosaicing

--- cpLabs/lab1/demosaic.py
+++ cpLabs/lab1/demosaic.py
@@ -7,12 +7,12 @@ def basic_demosaic(img, pattern_shift=[1,0]):
 
 	(height, width, channels) = img.shape
 	
+	# filling zeros for each color
 	for y in range(0, height):
 		for x in range(0, width):
 			color = __get_px_color(y,x,pattern_shift)
 			img[y,x,(color+1)%3] = img[y,x,(color-1)%3] = 0
 
-	# filling zeros for each color
 
 	out = img[:,:,1].astype(int) * 4
 	out[:-1,:] += img[1:,:,1]
@@ -20,7 +20,6 @@ def basic_demosaic(img, pattern_shift=[1,0]):
 	out[:,:-1] += img[:,1:,1]
 	out[:,1:] += img[:,:-1,1]
 	img[1:-1,1:-1,1] = out[1:-1,1:-1] / 4
-
 	
 	out = img[:,:,0].astype(int) * 2
 	out[:-1,:] += img[1:,:,0]
@@ -62,12 +61,61 @@ def slow_basic_demosaic(img, pattern_shift=[1,0]):
 				else:
 					img[y,x,2] = (int(img[y-1,x,2]) + int(img[y+1,x,2])) / 2
 					img[y,x,0] = (int(img[y,x-1,0]) + int(img[y,x+1,0])) / 2
-			elif __is_red_row(y, x, pattern_shift):
-				img[y,x,1] = (int(img[y-1,x,1]) + int(img[y+1,x,1]) + int(img[y,x-1,1]) + int(img[y,x+1,1])) / 4
-				img[y,x,0] = (int(img[y-1,x-1,0]) + int(img[y-1,x+1,0]) + int(img[y+1,x-1,0]) + int(img[y+1,x+1,0])) / 4
 			else:
 				img[y,x,1] = (int(img[y-1,x,1]) + int(img[y+1,x,1]) + int(img[y,x-1,1]) + int(img[y,x+1,1])) / 4
-				img[y,x,2] = (int(img[y-1,x-1,2]) + int(img[y-1,x+1,2]) + int(img[y+1,x-1,2]) + int(img[y+1,x+1,2])) / 4
+				if __is_red_row(y, x, pattern_shift):
+					img[y,x,0] = (int(img[y-1,x-1,0]) + int(img[y-1,x+1,0]) + int(img[y+1,x-1,0]) + int(img[y+1,x+1,0])) / 4
+				else:
+					img[y,x,2] = (int(img[y-1,x-1,2]) + int(img[y-1,x+1,2]) + int(img[y+1,x-1,2]) + int(img[y+1,x+1,2])) / 4
+
+def slow_edge_based_demosaic(img, pattern_shift=[1,0]):
+	if(len(img.shape) != 3 or img.shape[2] != 3):
+		raise NotImplementedError, "demosaic implemented only for three-channel images"
+
+	(height, width, channels) = img.shape
+	
+	# interpolate green
+
+	for y in range(1, height-1):
+		for x in range(1, width-1):
+			if not __is_green_px(y, x, pattern_shift):
+				gV = abs(int(img[y-1,x,1])-int(img[y+1,x,1]))
+				gH = abs(int(img[y,x-1,1])-int(img[y,x+1,1]))
+				if gV == gH:
+					img[y,x,1] = (int(img[y-1,x,1]) + int(img[y+1,x,1]) + int(img[y,x-1,1]) + int(img[y,x+1,1])) / 4
+				elif gV < gH:
+					img[y,x,1] = (int(img[y-1,x,1]) + int(img[y+1,x,1])) / 2
+				else:
+					img[y,x,1] = (int(img[y,x-1,1]) + int(img[y,x+1,1])) / 2
+
+	# interpolate R and B
+	# (interpolate R-G and add G)
+		
+	for y in range(1, height-1):
+		for x in range(1, width-1):
+			if __is_green_px(y, x, pattern_shift):
+				if __is_red_row(y, x, pattern_shift):
+					red = (int(img[y,x-1,2])-int(img[y,x-1,1])+int(img[y,x+1,2])-int(img[y,x+1,1]))/2 + int(img[y,x,1])
+					img[y,x,2] = max(red, 0)
+					blue = (int(img[y-1,x,0])-int(img[y-1,x,1])+int(img[y+1,x,0])-int(img[y+1,x,1]))/2 + int(img[y,x,1])
+					img[y,x,0] = max(blue, 0)
+				else:
+
+					blue = (int(img[y,x-1,0])-int(img[y,x-1,1])+int(img[y,x+1,0])-int(img[y,x+1,1]))/2 + int(img[y,x,1])
+					img[y,x,0] = max(blue, 0)
+					red = (int(img[y-1,x,2])-int(img[y-1,x,1])+int(img[y+1,x,2])-int(img[y+1,x,1]))/2 + int(img[y,x,1])
+					img[y,x,2] = max(red,0)
+
+			else:
+				if __is_red_row(y, x, pattern_shift):
+					blue = (int(img[y-1,x-1,0])-int(img[y-1,x-1,1]) + int(img[y-1,x+1,0])-int(img[y-1,x+1,1]) + int(img[y+1,x-1,0])-int(img[y+1,x-1,1]) + int(img[y+1,x+1,0])-int(img[y+1,x+1,1])) / 4 + int(img[y,x,1])
+					img[y,x,0] = max(blue,0)
+				else:
+					red = (int(img[y-1,x-1,2])-int(img[y-1,x-1,1]) + int(img[y-1,x+1,2])-int(img[y-1,x+1,1]) + int(img[y+1,x-1,2])-int(img[y+1,x-1,1]) + int(img[y+1,x+1,2])-int(img[y+1,x+1,1])) / 4 + int(img[y,x,1])
+					img[y,x,2] = max(red,0)
+
+
+
 
 def __is_green_px(y, x, pattern_shift):
 	return ((x+y)%2) == ((pattern_shift[0]+pattern_shift[1])%2)
--- cpLabs/lab1/t1.py
+++ cpLabs/lab1/t1.py
@@ -1,12 +1,14 @@
-# from cpLabs.lab1.demosaic import
 import cv2
-from cpLabs.lab1.demosaic import basic_demosaic, slow_basic_demosaic
+from cpLabs.lab1.demosaic import basic_demosaic, slow_basic_demosaic, slow_edge_based_demosaic
 
 def main():
-	img = cv2.imread('cpLabs/static/images/lighthouse_RAW_noisy_sigma0.01.png')
-	#img = cv2.imread('cpLabs/static/images/raw/text.png')
-	basic_demosaic(img, [0,1])
-	cv2.imshow('image',img)
+	img1 = cv2.imread('cpLabs/static/images/raw/signs-small.png')
+	slow_basic_demosaic(img1, [0,1])
+	cv2.imshow('image',img1)
+
+	img2 = cv2.imread('cpLabs/static/images/raw/signs-small.png')
+	slow_edge_based_demosaic(img2, [0,1])
+	cv2.imshow('image2',img2)
 	
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
