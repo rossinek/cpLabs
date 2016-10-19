4e26d73d21826a7c469d137a26a2ffe411d19f4e
Artur Rosa <rossinek@gmail.com>
2016-10-19 22:10:15 +0200

    menu in lab1

--- cpLabs/lab1/demosaic.py
+++ cpLabs/lab1/demosaic.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-def basic_demosaic(img, pattern_shift=[1,0]):
+def linear_demosaic(img, pattern_shift=[1,0]):
 	if(len(img.shape) != 3 or img.shape[2] != 3):
 		raise NotImplementedError, "demosaic implemented only for three-channel images"
 
@@ -13,60 +13,67 @@ def basic_demosaic(img, pattern_shift=[1,0]):
 			color = __get_px_color(y,x,pattern_shift)
 			img[y,x,(color+1)%3] = img[y,x,(color-1)%3] = 0
 
-
-	out = img[:,:,1].astype(int) * 4
-	out[:-1,:] += img[1:,:,1]
-	out[1:,:] += img[:-1,:,1]
-	out[:,:-1] += img[:,1:,1]
-	out[:,1:] += img[:,:-1,1]
-	img[1:-1,1:-1,1] = out[1:-1,1:-1] / 4
+	img_wide = cv2.copyMakeBorder(img,1,1,1,1,cv2.BORDER_REFLECT_101)
 	
-	out = img[:,:,0].astype(int) * 2
-	out[:-1,:] += img[1:,:,0]
-	out[1:,:] += img[:-1,:,0]
-	out[:,:-1] += img[:,1:,0]
-	out[:,1:] += img[:,:-1,0]
+	out = img_wide[:,:,1].astype(int) * 4
+	out[:-1,:] += img_wide[1:,:,1]
+	out[1:,:] += img_wide[:-1,:,1]
+	out[:,:-1] += img_wide[:,1:,1]
+	out[:,1:] += img_wide[:,:-1,1]
+	img_wide[1:-1,1:-1,1] = out[1:-1,1:-1] / 4
+	
+	out = img_wide[:,:,0].astype(int) * 2
+	out[:-1,:] += img_wide[1:,:,0]
+	out[1:,:] += img_wide[:-1,:,0]
+	out[:,:-1] += img_wide[:,1:,0]
+	out[:,1:] += img_wide[:,:-1,0]
 	out = out * 2
-	out[:-1,:-1] += img[1:,1:,0]
-	out[1:,1:] += img[:-1,:-1,0]
-	out[1:,:-1] += img[:-1,1:,0]
-	out[:-1,1:] += img[1:,:-1,0]
-	img[1:-1,1:-1,0] = out[1:-1,1:-1] / 4
+	out[:-1,:-1] += img_wide[1:,1:,0]
+	out[1:,1:] += img_wide[:-1,:-1,0]
+	out[1:,:-1] += img_wide[:-1,1:,0]
+	out[:-1,1:] += img_wide[1:,:-1,0]
+	img_wide[1:-1,1:-1,0] = out[1:-1,1:-1] / 4
 
-	out = img[:,:,2].astype(int) * 2	
-	out[:-1,:] += img[1:,:,2]
-	out[1:,:] += img[:-1,:,2]
-	out[:,:-1] += img[:,1:,2]
-	out[:,1:] += img[:,:-1,2]
+	out = img_wide[:,:,2].astype(int) * 2	
+	out[:-1,:] += img_wide[1:,:,2]
+	out[1:,:] += img_wide[:-1,:,2]
+	out[:,:-1] += img_wide[:,1:,2]
+	out[:,1:] += img_wide[:,:-1,2]
 	out = out * 2
-	out[:-1,:-1] += img[1:,1:,2]
-	out[1:,1:] += img[:-1,:-1,2]
-	out[1:,:-1] += img[:-1,1:,2]
-	out[:-1,1:] += img[1:,:-1,2]
-	img[1:-1,1:-1,2] = out[1:-1,1:-1] / 4
+	out[:-1,:-1] += img_wide[1:,1:,2]
+	out[1:,1:] += img_wide[:-1,:-1,2]
+	out[1:,:-1] += img_wide[:-1,1:,2]
+	out[:-1,1:] += img_wide[1:,:-1,2]
+	img_wide[1:-1,1:-1,2] = out[1:-1,1:-1] / 4
 
+	img[:,:] = img_wide[1:-1,1:-1]
 
-def slow_basic_demosaic(img, pattern_shift=[1,0]):
+def slow_linear_demosaic(img, pattern_shift=[1,0]):
 	if(len(img.shape) != 3 or img.shape[2] != 3):
 		raise NotImplementedError, "demosaic implemented only for three-channel images"
 
 	(height, width, channels) = img.shape
 	
-	for y in range(1, height-1):
-		for x in range(1, width-1):
-			if __is_green_px(y, x, pattern_shift):
-				if __is_red_row(y, x, pattern_shift):
-					img[y,x,2] = (int(img[y,x-1,2]) + int(img[y,x+1,2])) / 2
-					img[y,x,0] = (int(img[y-1,x,0]) + int(img[y+1,x,0])) / 2
+	img_wide = cv2.copyMakeBorder(img,1,1,1,1,cv2.BORDER_REFLECT_101)
+	# shift pattern shift
+	wide_pattern_shift = [s+1%2 for s in pattern_shift]
+
+	for y in range(1, height+1):
+		for x in range(1, width+1):
+			if __is_green_px(y, x, wide_pattern_shift):
+				if __is_red_row(y, x, wide_pattern_shift):
+					img_wide[y,x,2] = (int(img_wide[y,x-1,2]) + int(img_wide[y,x+1,2])) / 2
+					img_wide[y,x,0] = (int(img_wide[y-1,x,0]) + int(img_wide[y+1,x,0])) / 2
 				else:
-					img[y,x,2] = (int(img[y-1,x,2]) + int(img[y+1,x,2])) / 2
-					img[y,x,0] = (int(img[y,x-1,0]) + int(img[y,x+1,0])) / 2
+					img_wide[y,x,2] = (int(img_wide[y-1,x,2]) + int(img_wide[y+1,x,2])) / 2
+					img_wide[y,x,0] = (int(img_wide[y,x-1,0]) + int(img_wide[y,x+1,0])) / 2
 			else:
-				img[y,x,1] = (int(img[y-1,x,1]) + int(img[y+1,x,1]) + int(img[y,x-1,1]) + int(img[y,x+1,1])) / 4
-				if __is_red_row(y, x, pattern_shift):
-					img[y,x,0] = (int(img[y-1,x-1,0]) + int(img[y-1,x+1,0]) + int(img[y+1,x-1,0]) + int(img[y+1,x+1,0])) / 4
+				img_wide[y,x,1] = (int(img_wide[y-1,x,1]) + int(img_wide[y+1,x,1]) + int(img_wide[y,x-1,1]) + int(img_wide[y,x+1,1])) / 4
+				if __is_red_row(y, x, wide_pattern_shift):
+					img_wide[y,x,0] = (int(img_wide[y-1,x-1,0]) + int(img_wide[y-1,x+1,0]) + int(img_wide[y+1,x-1,0]) + int(img_wide[y+1,x+1,0])) / 4
 				else:
-					img[y,x,2] = (int(img[y-1,x-1,2]) + int(img[y-1,x+1,2]) + int(img[y+1,x-1,2]) + int(img[y+1,x+1,2])) / 4
+					img_wide[y,x,2] = (int(img_wide[y-1,x-1,2]) + int(img_wide[y-1,x+1,2]) + int(img_wide[y+1,x-1,2]) + int(img_wide[y+1,x+1,2])) / 4
+	img[:,:] = img_wide[1:-1,1:-1]
 
 def slow_edge_based_demosaic(img, pattern_shift=[1,0]):
 	if(len(img.shape) != 3 or img.shape[2] != 3):
@@ -74,48 +81,50 @@ def slow_edge_based_demosaic(img, pattern_shift=[1,0]):
 
 	(height, width, channels) = img.shape
 	
-	# interpolate green
+	img_wide = cv2.copyMakeBorder(img,1,1,1,1,cv2.BORDER_REFLECT_101)
+	# shift pattern shift
+	wide_pattern_shift = [s+1%2 for s in pattern_shift]
 
-	for y in range(1, height-1):
-		for x in range(1, width-1):
-			if not __is_green_px(y, x, pattern_shift):
-				gV = abs(int(img[y-1,x,1])-int(img[y+1,x,1]))
-				gH = abs(int(img[y,x-1,1])-int(img[y,x+1,1]))
+	# interpolate green
+	for y in range(1, height+1):
+		for x in range(1, width+1):
+			if not __is_green_px(y, x, wide_pattern_shift):
+				gV = abs(int(img_wide[y-1,x,1])-int(img_wide[y+1,x,1]))
+				gH = abs(int(img_wide[y,x-1,1])-int(img_wide[y,x+1,1]))
 				if gV == gH:
-					img[y,x,1] = (int(img[y-1,x,1]) + int(img[y+1,x,1]) + int(img[y,x-1,1]) + int(img[y,x+1,1])) / 4
+					img_wide[y,x,1] = (int(img_wide[y-1,x,1]) + int(img_wide[y+1,x,1]) + int(img_wide[y,x-1,1]) + int(img_wide[y,x+1,1])) / 4
 				elif gV < gH:
-					img[y,x,1] = (int(img[y-1,x,1]) + int(img[y+1,x,1])) / 2
+					img_wide[y,x,1] = (int(img_wide[y-1,x,1]) + int(img_wide[y+1,x,1])) / 2
 				else:
-					img[y,x,1] = (int(img[y,x-1,1]) + int(img[y,x+1,1])) / 2
+					img_wide[y,x,1] = (int(img_wide[y,x-1,1]) + int(img_wide[y,x+1,1])) / 2
 
 	# interpolate R and B
 	# 	- interpolate R-G or B-G
 	#	- add G
-
-	for y in range(1, height-1):
-		for x in range(1, width-1):
-			if __is_green_px(y, x, pattern_shift):
-				if __is_red_row(y, x, pattern_shift):
-					red = (int(img[y,x-1,2])-int(img[y,x-1,1])+int(img[y,x+1,2])-int(img[y,x+1,1]))/2 + int(img[y,x,1])
-					img[y,x,2] = __constrain_uint8(red)
-					blue = (int(img[y-1,x,0])-int(img[y-1,x,1])+int(img[y+1,x,0])-int(img[y+1,x,1]))/2 + int(img[y,x,1])
-					img[y,x,0] = __constrain_uint8(blue)
+	for y in range(1, height+1):
+		for x in range(1, width+1):
+			if __is_green_px(y, x, wide_pattern_shift):
+				if __is_red_row(y, x, wide_pattern_shift):
+					red = (int(img_wide[y,x-1,2])-int(img_wide[y,x-1,1])+int(img_wide[y,x+1,2])-int(img_wide[y,x+1,1]))/2 + int(img_wide[y,x,1])
+					img_wide[y,x,2] = __constrain_uint8(red)
+					blue = (int(img_wide[y-1,x,0])-int(img_wide[y-1,x,1])+int(img_wide[y+1,x,0])-int(img_wide[y+1,x,1]))/2 + int(img_wide[y,x,1])
+					img_wide[y,x,0] = __constrain_uint8(blue)
 				else:
 
-					blue = (int(img[y,x-1,0])-int(img[y,x-1,1])+int(img[y,x+1,0])-int(img[y,x+1,1]))/2 + int(img[y,x,1])
-					img[y,x,0] = __constrain_uint8(blue)
-					red = (int(img[y-1,x,2])-int(img[y-1,x,1])+int(img[y+1,x,2])-int(img[y+1,x,1]))/2 + int(img[y,x,1])
-					img[y,x,2] = __constrain_uint8(red)
+					blue = (int(img_wide[y,x-1,0])-int(img_wide[y,x-1,1])+int(img_wide[y,x+1,0])-int(img_wide[y,x+1,1]))/2 + int(img_wide[y,x,1])
+					img_wide[y,x,0] = __constrain_uint8(blue)
+					red = (int(img_wide[y-1,x,2])-int(img_wide[y-1,x,1])+int(img_wide[y+1,x,2])-int(img_wide[y+1,x,1]))/2 + int(img_wide[y,x,1])
+					img_wide[y,x,2] = __constrain_uint8(red)
 
 			else:
-				if __is_red_row(y, x, pattern_shift):
-					blue = (int(img[y-1,x-1,0])-int(img[y-1,x-1,1]) + int(img[y-1,x+1,0])-int(img[y-1,x+1,1]) + int(img[y+1,x-1,0])-int(img[y+1,x-1,1]) + int(img[y+1,x+1,0])-int(img[y+1,x+1,1])) / 4 + int(img[y,x,1])
-					img[y,x,0] = __constrain_uint8(blue)
+				if __is_red_row(y, x, wide_pattern_shift):
+					blue = (int(img_wide[y-1,x-1,0])-int(img_wide[y-1,x-1,1]) + int(img_wide[y-1,x+1,0])-int(img_wide[y-1,x+1,1]) + int(img_wide[y+1,x-1,0])-int(img_wide[y+1,x-1,1]) + int(img_wide[y+1,x+1,0])-int(img_wide[y+1,x+1,1])) / 4 + int(img_wide[y,x,1])
+					img_wide[y,x,0] = __constrain_uint8(blue)
 				else:
-					red = (int(img[y-1,x-1,2])-int(img[y-1,x-1,1]) + int(img[y-1,x+1,2])-int(img[y-1,x+1,1]) + int(img[y+1,x-1,2])-int(img[y+1,x-1,1]) + int(img[y+1,x+1,2])-int(img[y+1,x+1,1])) / 4 + int(img[y,x,1])
-					img[y,x,2] = __constrain_uint8(red)
+					red = (int(img_wide[y-1,x-1,2])-int(img_wide[y-1,x-1,1]) + int(img_wide[y-1,x+1,2])-int(img_wide[y-1,x+1,1]) + int(img_wide[y+1,x-1,2])-int(img_wide[y+1,x-1,1]) + int(img_wide[y+1,x+1,2])-int(img_wide[y+1,x+1,1])) / 4 + int(img_wide[y,x,1])
+					img_wide[y,x,2] = __constrain_uint8(red)
 
-
+	img[:,:] = img_wide[1:-1,1:-1]
 
 
 def __is_green_px(y, x, pattern_shift):
--- cpLabs/lab1/t1.py
+++ cpLabs/lab1/t1.py
@@ -1,12 +1,12 @@
 import cv2
-from cpLabs.lab1.demosaic import basic_demosaic, slow_basic_demosaic, slow_edge_based_demosaic
+from cpLabs.lab1.demosaic import linear_demosaic, slow_linear_demosaic, slow_edge_based_demosaic
 from cpLabs.lab1.gamma_correction import gamma_correction
 
 def main():
-	img1 = cv2.imread('cpLabs/static/images/lighthouse_RAW_noisy_sigma0.01.png')
-	slow_basic_demosaic(img1)
+	'''img1 = cv2.imread('cpLabs/static/images/lighthouse_RAW_noisy_sigma0.01.png')
+	slow_linear_demosaic(img1)
 	gamma_correction(img1, 2.2)
-	cv2.imshow('image',img1)
+	cv2.imshow('image',	img1)
 
 	img2 = cv2.imread('cpLabs/static/images/lighthouse_RAW_noisy_sigma0.01.png')
 	slow_edge_based_demosaic(img2)
@@ -15,5 +15,116 @@ def main():
 	
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
+	'''
+	while True:
+		selection = __image_menu()
+		if selection:
+			repeat = __action_menu(selection[0], selection[1])
+			if not repeat:
+				return
+		else:
+			return
+
+
+
+
+
+def __action_menu(img, pattern_shift):
+	img = cv2.imread('cpLabs/static/images/lighthouse_RAW_noisy_sigma0.01.png')
+	img_cp = img.copy()
+
+	options = [
+		['slow_linear_demosaic'],
+		['linear_demosaic'],
+		['slow_edge_based_demosaic'],
+		['gamma_correction'],
+		['Reset image'],
+		['Change image'],
+		['Exit']]
+
+	while True: 
+		print 'Choose action'
+		for i in range(0, len(options)): 
+			print str(i+1)+'.', options[i][0]
+
+		input_string = raw_input('>>> ') 
+		try:
+			option = int(input_string)-1
+		except ValueError:
+			option = -1
+
+		if option in range(0,len(options)):
+			print '>', options[option][0]
+
+		if option==0:
+			slow_linear_demosaic(img)
+			__show_image(img)
+		elif option==1:
+			linear_demosaic(img)
+			__show_image(img)
+		elif option==2:
+			slow_edge_based_demosaic(img)
+			__show_image(img)
+		elif option==3:
+			gamma = __input_gamma()
+			gamma_correction(img, gamma)
+			__show_image(img)
+		elif option==len(options)-3: 
+			img=img_cp.copy()
+		elif option==len(options)-2: 
+			return True
+		elif option==len(options)-1: 
+			return False
+		else: 
+			print '> Unknown option selected!'
+			continue
+
+def __input_gamma():
+	while True: 
+		print 'Input gamma (0.0 < x < 100.0))'
+		input_string = raw_input('>>> ') 
+		try:
+			option = float(input_string)
+		except ValueError:
+			option = None
+		
+		if option:
+			return option
+
+def __image_menu():
+	options = [
+		['lighthouse_RAW_noisy_sigma0.01.png', 'cpLabs/static/images/lighthouse_RAW_noisy_sigma0.01.png', [1,0]],
+		['signs.png', 'cpLabs/static/images/raw/signs.png', [0,1]],
+		['signs-small.png', 'cpLabs/static/images/raw/signs-small.png', [0,1]],
+		['text.png', 'cpLabs/static/images/raw/text.png', [0,1]],
+		['text2.png', 'cpLabs/static/images/raw/text2.png', [0,1]],
+		['Exit']]
+
+	while True: 
+		print 'Choose image'
+		for i in range(0, len(options)): 
+			print str(i+1)+'.', options[i][0]
+
+		input_string = raw_input('>>> ') 
+		try:
+			option = int(input_string)-1
+		except ValueError:
+			option = -1
+
+		if option in range(0, len(options)-1):
+			print '>', options[option][0]
+			return options[option][1:]
+		elif option==len(options)-1: 
+			print '> Exit'
+			return None
+		else: 
+			print 'Unknown option selected!'
+			continue
+
+
+def __show_image(img):
+	cv2.imshow('Image',img)
+	cv2.waitKey(0)
+	cv2.destroyWindow('Image')
 
 main()
