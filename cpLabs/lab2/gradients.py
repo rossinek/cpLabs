ceddce2bf674e37e39b1674bfece4cae70f63839
Artur Rosa <rossinek@gmail.com>
2016-10-23 01:38:57 +0200

    working prototype

--- cpLabs/lab2/gradients.py
+++ cpLabs/lab2/gradients.py
@@ -2,8 +2,20 @@ import cv2
 import numpy as np
 from scipy import ndimage
 
-GRADIENT_X_OPERATOR = np.array([[-1, 0, 1]])
-GRADIENT_Y_OPERATOR = np.array([[-1], [0], [1]])
+SIMPLE_GRADIENT_X_OPERATOR = np.array([[-1, 0, 1]])
+SIMPLE_GRADIENT_Y_OPERATOR = np.array([[-1], [0], [1]])
+
+SOBEL_X_OPERATOR = np.array([
+	[-1, 0, 1],
+	[-2, 0, 2],
+	[-1, 0, 1],
+	])
+
+SOBEL_Y_OPERATOR = np.array([
+	[-1,-2, -1],
+	[0,  0,  0],
+	[1,  2,  1],
+	])
 
 def compute_gradients(img):
 	if(len(img.shape) != 2):
@@ -11,7 +23,18 @@ def compute_gradients(img):
 	(height, width) = img.shape
 	
 	data = np.array(img, dtype=float)
-	gradients_x = ndimage.convolve(data, GRADIENT_X_OPERATOR)
-	gradients_y = ndimage.convolve(data, GRADIENT_Y_OPERATOR)
+	gradients_x = ndimage.convolve(data, SIMPLE_GRADIENT_X_OPERATOR)
+	gradients_y = ndimage.convolve(data, SIMPLE_GRADIENT_Y_OPERATOR)
+
+	return np.absolute(gradients_x+gradients_y)
+
+def compute_sobel_gradients(img):
+	if(len(img.shape) != 2):
+		raise NotImplementedError, "compute_gradients implemented only for one-channel images"
+	(height, width) = img.shape
+	
+	data = np.array(img, dtype=float)
+	gradients_x = ndimage.convolve(data, SOBEL_X_OPERATOR)
+	gradients_y = ndimage.convolve(data, SOBEL_Y_OPERATOR)
 
 	return np.absolute(gradients_x+gradients_y)
--- cpLabs/lab2/t1.py
+++ cpLabs/lab2/t1.py
@@ -1,27 +1,58 @@
 import cv2
+import numpy as np
 from operator import itemgetter
 
-from cpLabs.lab2.gradients import compute_gradients
+from cpLabs.lab2.gradients import compute_gradients, compute_sobel_gradients
 
 def main():
 	print "Task 1: Focal Stack"
 	img1 = cv2.imread("cpLabs/static/images/focalstack/stack1.png", 0)
 	img2 = cv2.imread("cpLabs/static/images/focalstack/stack2.png", 0)
 	img3 = cv2.imread("cpLabs/static/images/focalstack/stack3.png", 0)
+	(height, width) = img1.shape
 
 	images = [img1,img2,img3]
-	gradients = [compute_gradients(img1), compute_gradients(img2), compute_gradients(img3)]
-	output = img1.copy()
-	(height, width) = img1.shape
+	g_simple = [compute_gradients(img1), compute_gradients(img2), compute_gradients(img3)]
+	g_sobel = [compute_sobel_gradients(img1), compute_sobel_gradients(img2), compute_sobel_gradients(img3)]
+	
+	contrib_simple = [np.zeros((height, width), np.uint8), np.zeros((height, width), np.uint8), np.zeros((height, width), np.uint8)]
+	contrib_sobel = [np.zeros((height, width), np.uint8), np.zeros((height, width), np.uint8), np.zeros((height, width), np.uint8)]
+
+	output_simple = np.zeros((height, width), np.uint8)
 	for y in range(0, height):
 		for x in range(0, width):
-			argmax = __argmax([gradients[0][y,x], gradients[1][y,x], gradients[2][y,x]])
-			output[y,x] = images[argmax][y,x]
+			argmax = __argmax([g_simple[0][y,x], g_simple[1][y,x], g_simple[2][y,x]])
+			output_simple[y,x] = images[argmax][y,x]
+			contrib_simple[argmax][y,x] = 255
 
-	cv2.imshow('Image',output)
-	cv2.imshow('img1',img1)
-	cv2.imshow('img2',img2)
-	cv2.imshow('img3',img3)
+	output_sobel = np.zeros((height, width), np.uint8)
+	for y in range(0, height):
+		for x in range(0, width):
+			argmax = __argmax([g_sobel[0][y,x], g_sobel[1][y,x], g_sobel[2][y,x]])
+			output_sobel[y,x] = images[argmax][y,x]
+			contrib_sobel[argmax][y,x] = 255
+	
+	# display
+	images_disp = np.zeros((height, 3*width), np.uint8)
+	images_disp[:height, :width] = img1
+	images_disp[:height, width:2*width] = img2
+	images_disp[:height, 2*width:3*width] = img3
+
+	simple_disp = np.zeros((height, 4*width), np.uint8)
+	simple_disp[:height, :width] = contrib_simple[0]
+	simple_disp[:height, width:2*width] = contrib_simple[1]
+	simple_disp[:height, 2*width:3*width] = contrib_simple[2]
+	simple_disp[:height, 3*width:4*width] = output_simple
+
+	sobel_disp = np.zeros((height, 4*width), np.uint8)
+	sobel_disp[:height, :width] = contrib_sobel[0]
+	sobel_disp[:height, width:2*width] = contrib_sobel[1]
+	sobel_disp[:height, 2*width:3*width] = contrib_sobel[2]
+	sobel_disp[:height, 3*width:4*width] = output_sobel
+
+	cv2.imshow("Original images", images_disp)
+	cv2.imshow('Output (simple gradient operator)',simple_disp)
+	cv2.imshow('Output (sobel operator)', sobel_disp)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
