1ba59f6c53655d97c2c4313d8fa916270fccc802
Artur Rosa <rossinek@gmail.com>
2016-10-25 02:47:58 +0200

    t1 crossing

--- cpLabs/lab2/t1.py
+++ cpLabs/lab2/t1.py
@@ -12,17 +12,27 @@ def main():
 		cv2.imread("cpLabs/static/images/focalstack/stack3.png", 0)
 	]
 
-	output_simple, contrib_simple = all_in_focus_crossing(images, SIMPLE_OPERATOR_FLAG)
-	output_sobel, contrib_sobel = all_in_focus_crossing(images, SOBEL_OPERATOR_FLAG)
+	output_simple, contrib_simple = all_in_focus(images, SIMPLE_OPERATOR_FLAG)
+	output_sobel, contrib_sobel = all_in_focus(images, SOBEL_OPERATOR_FLAG)
+
+	output_simple_crossing, contrib_simple_crossing = all_in_focus_crossing(images, SIMPLE_OPERATOR_FLAG)
+	output_sobel_crossing, contrib_sobel_crossing = all_in_focus_crossing(images, SOBEL_OPERATOR_FLAG)
 
 	display_array = [
 		images,
 		[contrib_simple[0], contrib_simple[1], contrib_simple[2], output_simple],
-		[contrib_sobel[0], contrib_sobel[1], contrib_sobel[2], output_sobel]
+		[contrib_sobel[0], contrib_sobel[1], contrib_sobel[2], output_sobel],
+	]
+
+	display_array_crossing = [
+	[contrib_simple_crossing[0], contrib_simple_crossing[1], contrib_simple_crossing[2], output_simple_crossing],
+		[contrib_sobel_crossing[0], contrib_sobel_crossing[1], contrib_sobel_crossing[2], output_sobel_crossing]
 	]
 	
 	all_in_one_display = multiple_display(display_array)
+	all_in_one_display_crossing = multiple_display(display_array_crossing)
 	cv2.imshow('original, simple, sobel', all_in_one_display)
+	cv2.imshow('simple, sobel (with crossing', all_in_one_display_crossing)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
