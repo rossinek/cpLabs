e083df912fff6465840c6e10f24ccfe7aa3967eb
Artur Rosa <rossinek@gmail.com>
2016-11-02 22:13:58 +0100

    t2, t3 from lab2

--- cpLabs/lab2/t2.py
+++ cpLabs/lab2/t2.py
@@ -6,50 +6,25 @@ from cpLabs.lib.hdr import simple_robertson
 
 def main():
 	print "Task 2: HDR"
+
 	images = [
-		cv2.imread("cpLabs/static/images/Memorial_SourceImages/memorial0061.png"),
-		cv2.imread("cpLabs/static/images/Memorial_SourceImages/memorial0062.png"),
-		cv2.imread("cpLabs/static/images/Memorial_SourceImages/memorial0063.png"),
-		cv2.imread("cpLabs/static/images/Memorial_SourceImages/memorial0064.png"),
-		cv2.imread("cpLabs/static/images/Memorial_SourceImages/memorial0065.png"),
-		cv2.imread("cpLabs/static/images/Memorial_SourceImages/memorial0066.png"),
-		cv2.imread("cpLabs/static/images/Memorial_SourceImages/memorial0067.png"),
-		cv2.imread("cpLabs/static/images/Memorial_SourceImages/memorial0068.png"),
-		cv2.imread("cpLabs/static/images/Memorial_SourceImages/memorial0069.png"),
-		cv2.imread("cpLabs/static/images/Memorial_SourceImages/memorial0070.png"),
-		cv2.imread("cpLabs/static/images/Memorial_SourceImages/memorial0071.png"),
-		cv2.imread("cpLabs/static/images/Memorial_SourceImages/memorial0072.png"),
-		cv2.imread("cpLabs/static/images/Memorial_SourceImages/memorial0073.png"),
-		cv2.imread("cpLabs/static/images/Memorial_SourceImages/memorial0074.png"),
-		cv2.imread("cpLabs/static/images/Memorial_SourceImages/memorial0075.png"),
-		cv2.imread("cpLabs/static/images/Memorial_SourceImages/memorial0076.png")
+		cv2.imread("cpLabs/static/images/my_hdr/_DSC00051.JPG"),
+		cv2.imread("cpLabs/static/images/my_hdr/_DSC00052.JPG"),
+		cv2.imread("cpLabs/static/images/my_hdr/_DSC00053.JPG"),
+		cv2.imread("cpLabs/static/images/my_hdr/_DSC00054.JPG"),
+		cv2.imread("cpLabs/static/images/my_hdr/_DSC00055.JPG")
 	]
 
 	exposures = [
-		1.0/0.03125,
-		1.0/0.0625,
-		1.0/0.125,
-		1.0/0.25,
-		1.0/0.5,
-		1.0/1.0,
-		1.0/2.0,
-		1.0/4.0,
-		1.0/8.0,
-		1.0/16.0,
-		1.0/32.0,
-		1.0/64.0,
-		1.0/128.0,
-		1.0/256.0,
-		1.0/512.0,
-		1.0/1024.0
+		1.0/50.0,
+		1.0/80.0,
+		1.0/30.0,
+		1.0/125.0,
+		1.0/20.0
 	]
 	
 	output = simple_robertson(images, exposures)
 
-	cv2.imwrite('output/memorial.exr',output/255.)
-	
-	#cv2.imshow('Image1', output/255.)
-	#cv2.waitKey(0)
-	#cv2.destroyAllWindows()
+	cv2.imwrite('output/myhdr.exr',output/255.)
 
 main()
--- cpLabs/lab2/t3.py
+++ cpLabs/lab2/t3.py
@@ -7,14 +7,18 @@ from cpLabs.lib.hdr import bilateral_filter_tone_mapping
 def main():
 	print "Task 3: Tone mapping"
 	image = cv2.imread("cpLabs/static/images/hdr/memorial.exr", cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
-	#image = cv2.imread("output/memorial.exr", cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
+	image2 = cv2.imread("output/myhdr.exr", cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
 
-	output1 = bilateral_filter_tone_mapping(image, -1, 0.4, 12, 5)
-	output2 = bilateral_filter_tone_mapping(image, -1, 0.5, 12, 5)
-	output3 = bilateral_filter_tone_mapping(image, -1, 0.6, 12, 5)
+	output_1_1 = bilateral_filter_tone_mapping(image, -1, 0.2, 12, 8)
+	output_1_2 = bilateral_filter_tone_mapping(image, -1, 0.5, 12, 8)
+	output_1_3 = bilateral_filter_tone_mapping(image, -1, 0.8, 12, 8)
 
-	print np.amin(image), np.amax(image)
-	cv2.imshow('Image1', multiple_display([[image, output1, output2, output3]]))
+	output_2_1 = bilateral_filter_tone_mapping(image2, -1, 0.2, 4, 200)
+	output_2_2 = bilateral_filter_tone_mapping(image2, -1, 0.7, 4, 200)
+	output_2_3 = bilateral_filter_tone_mapping(image2, -1, 3.2, 4, 200)
+
+	cv2.imshow('Image1', multiple_display([[output_1_1, output_1_2, output_1_3]]))
+	cv2.imshow('Image2', multiple_display([[output_2_1, output_2_2, output_2_3]]))
 
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
