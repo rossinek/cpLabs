db5cd75e25783b67951d73bed5c562863cdb9461
Artur Rosa <rossinek@gmail.com>
2016-11-22 10:14:26 +0100

    lab5

--- cpLabs/lib/display_helper.py
+++ cpLabs/lib/display_helper.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 def multiple_display(array):
 	sizes = []
@@ -26,3 +27,27 @@ def multiple_display(array):
 		row_pos+=sizes[y][0]
 
 	return output_display
+
+def draw_point(img, A):
+	cv2.rectangle(img, (A[0]-1,A[1]-1), (A[0]+1,A[1]+1), (0,255,0), -1)
+
+def draw_arrow(img, A, B):
+	cv2.line(img, A, B, (255,0,0), 1, cv2.CV_AA)
+	cv2.rectangle(img, (A[0]-1,A[1]-1), (A[0]+1,A[1]+1), (0,255,0), -1)
+	cv2.rectangle(img, (B[0]-1,B[1]-1), (B[0]+1,B[1]+1), (0,0,255), -1)
+
+def draw_circle_on_center(img):
+	(h, w, c) = img.shape
+	center = (w/2, h/2)
+	radius = min(w/2, h/2)
+	cv2.circle(img, center, radius, (255, 255, 255))
+
+def draw_rectangle_on_center(img):
+	(h, w, c) = img.shape
+	center = (w/2, h/2)
+	rw = w
+	rh = w/2
+	cv2.rectangle(img, (0,h/2-rh/2), (w-1,h/2+rh/2), (255, 255, 255))
+
+def normalized_copy(img):
+	return img*(255./np.max(img))
